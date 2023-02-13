9bbbfc6c7d771b462a58f4503bbae0175704599b
baser <baserph@yandex.ru>
2023-02-13 23:32:58 +0300

    Ещё чуть правок

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -1,13 +1,16 @@
 package ru.practicum.shareit;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class ShareItApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShareItApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ShareItApp.class, args);
+        log.info("Проходит тесты постман, почему-то падает в гите");
+    }
 
 }
