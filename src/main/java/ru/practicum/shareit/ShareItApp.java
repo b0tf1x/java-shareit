813de413f58d882163aa200aefa1614d18160eff
baser <baserph@yandex.ru>
2023-02-28 23:05:33 +0300

    После правок 2

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -11,5 +11,6 @@ public class ShareItApp {
     public static void main(String[] args) {
         SpringApplication.run(ShareItApp.class, args);
     }
+
     public static final String USER_HEADER = "X-Sharer-User-Id";
 }
