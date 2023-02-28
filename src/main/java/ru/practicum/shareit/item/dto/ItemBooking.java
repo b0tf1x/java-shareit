a3d644101e9af246413351212e969ba97899de85
baser <baserph@yandex.ru>
2023-02-28 21:38:50 +0300

    После правок

--- src/main/java/ru/practicum/shareit/item/dto/ItemBooking.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemBooking.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.item.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.comment.CommentDto;
 
