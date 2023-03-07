30c0157e02117297645da08ed7c97bae48de615e
baser <baserph@yandex.ru>
2023-03-07 19:07:02 +0300

    правки code-style

--- server/src/main/java/ru/practicum/shareit/ShareItServer.java
+++ server/src/main/java/ru/practicum/shareit/ShareItServer.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ShareItServer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShareItServer.class, args);
-	}
-	public static final String USER_HEADER = "X-Sharer-User-Id";
+    public static void main(String[] args) {
+        SpringApplication.run(ShareItServer.class, args);
+    }
+
+    public static final String USER_HEADER = "X-Sharer-User-Id";
 }
